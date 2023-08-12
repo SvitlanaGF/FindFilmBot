7e73e5f5f6a78b00b29ee0898f3e3fccc780db72
Svitlana <svitlanagayduchyk76@gmail.com>
2023-08-12 18:09:28 +0300

    add function for finding an information about the actor

--- scrape.py
+++ scrape.py
@@ -1,20 +1,19 @@
-import time
-
 from bs4 import BeautifulSoup
 import requests
 import re
 import numpy as np
+import pywhatkit as pwt
 from collections import Counter
 # https://megogo.net/ua/search-extended?category_id=16&country_id=ua&main_tab=filters&sort=rating_imdb&video_genre_id=comedy
 
-# class Scrape:
-#     def __init__(self, url):
-#         self.url = url
-#
-#     def soup_(self):
-#         response = requests.get(url=self.url)
-#         soup = BeautifulSoup(response.text, 'html.parser')
-#         return soup
+class Scrape:
+    def __init__(self, url):
+        self.url = url
+
+    def soup_(self):
+        response = requests.get(url=self.url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        return soup
 
 def find_top(video_genre_id = None, ua=False):
     try:
@@ -32,6 +31,23 @@ def find_top(video_genre_id = None, ua=False):
     except Exception as e:
         print(e)
 
-
+def find_actor(actor_name:str):
+    try:
+        url = f"https://en.wikipedia.org/wiki/{'_'.join(actor_name.title().split(' '))}"
+        response = requests.get(url=url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        find = soup('table', {'class':"infobox"})
+        bio = []
+        bio.append(f'Name --- {actor_name.title()}\n---------------------------')
+        for i in find:
+            for j in i.find_all('tr'):
+                h = j.find_all('th')
+                d = j.find_all('td')
+                if h is not None and d is not None:
+                    for k,v in zip(h,d):
+                        bio.append(f'{k.text} --- {v.text}\n---------------------------')
+        return list(bio)
+    except Exception as e:
+        print(e)
 # re.search("<h3.+>(.+)</h3>",str(name)).group(0)
 
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,4 +1,3 @@
-from typing import Final
 from telegram import Update, Bot
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, Updater
 from scrape import *
@@ -7,7 +6,7 @@ TOKEN = 'token'
 bot_usname = '@Filmother_bot'
 # find information about the movie
 # find information about actors from the film
-# find information about the actor
+# find information about the actor +++
 # find information about tops +++
 
 
@@ -15,33 +14,30 @@ bot_usname = '@Filmother_bot'
 # #
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Hello! I`m Filmother. I know everything(or almost everything🙂) about movies🎥. '
-                                    'So, I`m ready to help you😇.\nHere all my commands:\n/help --> all commands\n/film_list --> list of movies by genre\n')
-#     # return f'Hello, {update.message.chat.id}!'
-#
-# async def custom(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     # await update.message.reply_text('Write something')
-#     txt = update.message.text
-#     return f'Your message is {txt}'
-# #
-# #
+                                    'So, I`m ready to help you😇.\nHere all my commands:\n/help --> all commands\n/film_list --> list of movies by genre\n/actor --> information about the actor')
+
+
 def cust(txt):
     if txt == 'hello':
         return 't'
+
+
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(f'There is error {context.error} from {update}')
 
+
 async def custom(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await app.add_handler(MessageHandler(update.message.text, cust))
-# #
-# #
-# # def information_about_film(movie_name):
-# #     pass
+
 
 async def tops(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Write one of movie genre in the format:"Genre: {your genre}" \n(if you need only ukrainian movies, write "ua" after genre, example:"Genre: travel, ua"):\n🎞Comedy🎭\n🎞Entertaining😁\n🎞Action🔫\n🎞Detective🕵️‍♀\n🎞Melodrama❤️\n🎞Thriller😬\n🎞Horror🙀\n🎞Musical🎵\n🎞Western🤠\n🎞Adventures😎\n🎞Sport🤾‍♀️\n🎞Sci-Fi👽\n🎞Crime👮‍♀️\n🎞Drama❤️\n🎞Short🎬\n🎞History🤴\n🎞Documentary🌟\n🎞Family👨‍👩‍👧‍👦\n🎞Cooking🍪\n🎞Travel🌴\n🎞Kids👶\n🎞Comics🦸‍♀️\n🎞Fantasy🧚‍♂️\n🎞All🎥')
 
-async def find_information_about_movie(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+async def find_information_about_actor(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Write name and surname of an actor in te format:"Actor/Actress: {name surname}"')
+
+
 def responses(text):
     text = text.lower()
     if 'hello' in text:
@@ -59,7 +55,11 @@ def responses(text):
         if text.endswith('ua'):
             ua = True
         return 'Films:\n🌟'+'\n\n🌟'.join(find_top(video_genre_id=genre, ua=ua))
-# #
+    elif text.startswith('actor'):
+        actor = text[text.index(':')+1:].strip()
+        return 'Actor:\n'+'\n'.join(find_actor(actor_name=actor))
+
+
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message_type = update.message.chat.type
     txt = update.message.text
@@ -80,6 +80,7 @@ if __name__ == '__main__':
     app.add_handler(CommandHandler('help', help))
     app.add_handler(CommandHandler('custom', custom))
     app.add_handler(CommandHandler("film_list", tops))
+    app.add_handler(CommandHandler('actor', find_information_about_actor))
 
     app.add_handler(MessageHandler(filters.TEXT, handle_message))
     app.add_error_handler(error)
