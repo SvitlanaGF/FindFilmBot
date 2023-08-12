01f3c953420b7bad27e40217f09b077326a53090
Svitlana <svitlanagayduchyk76@gmail.com>
2023-08-12 18:44:25 +0300

    add finding an information about the film

--- scrape.py
+++ scrape.py
@@ -2,18 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import numpy as np
-import pywhatkit as pwt
-from collections import Counter
-# https://megogo.net/ua/search-extended?category_id=16&country_id=ua&main_tab=filters&sort=rating_imdb&video_genre_id=comedy
 
-class Scrape:
-    def __init__(self, url):
-        self.url = url
-
-    def soup_(self):
-        response = requests.get(url=self.url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        return soup
 
 def find_top(video_genre_id = None, ua=False):
     try:
@@ -31,14 +20,19 @@ def find_top(video_genre_id = None, ua=False):
     except Exception as e:
         print(e)
 
-def find_actor(actor_name:str):
+
+def find_(name:str, actor=True):
     try:
-        url = f"https://en.wikipedia.org/wiki/{'_'.join(actor_name.title().split(' '))}"
+        if actor == True:
+            n = 'Name'
+        else:
+            n = 'Title'
+        url = f"https://en.wikipedia.org/wiki/{'_'.join(name.title().split(' '))}"
         response = requests.get(url=url)
         soup = BeautifulSoup(response.text, 'html.parser')
         find = soup('table', {'class':"infobox"})
         bio = []
-        bio.append(f'Name --- {actor_name.title()}\n---------------------------')
+        bio.append(f'{n} --- {name.title()}\n---------------------------')
         for i in find:
             for j in i.find_all('tr'):
                 h = j.find_all('th')
@@ -49,5 +43,6 @@ def find_actor(actor_name:str):
         return list(bio)
     except Exception as e:
         print(e)
+
 # re.search("<h3.+>(.+)</h3>",str(name)).group(0)
 
--- telegram_bot.py
+++ telegram_bot.py
@@ -4,30 +4,19 @@ from scrape import *
 
 TOKEN = 'token'
 bot_usname = '@Filmother_bot'
-# find information about the movie
-# find information about actors from the film
+# find information about the movie +++
 # find information about the actor +++
 # find information about tops +++
 
 
-#
-# #
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Hello! I`m Filmother. I know everything(or almost everything🙂) about movies🎥. '
                                     'So, I`m ready to help you😇.\nHere all my commands:\n/help --> all commands\n/film_list --> list of movies by genre\n/actor --> information about the actor')
 
 
-def cust(txt):
-    if txt == 'hello':
-        return 't'
-
-
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(f'There is error {context.error} from {update}')
 
-
-async def custom(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await app.add_handler(MessageHandler(update.message.text, cust))
 
 
 async def tops(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -38,11 +27,15 @@ async def find_information_about_actor(update: Update, context: ContextTypes.DEF
     await update.message.reply_text('Write name and surname of an actor in te format:"Actor/Actress: {name surname}"')
 
 
+async def find_information_about_movie(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Write movie title in te format:"Movie: {title}"')
+
+
 def responses(text):
     text = text.lower()
     if 'hello' in text:
-        return cust(text)
-    elif text.startswith('genre'):
+        return 'Hi!👋'
+    elif text.startswith('genre') and len(text[text.index(':')+1:].strip())!=0:
         text_elems = text[6:].split(',')
         ua = False
         if text_elems[0].strip() in \
@@ -55,16 +48,20 @@ def responses(text):
         if text.endswith('ua'):
             ua = True
         return 'Films:\n🌟'+'\n\n🌟'.join(find_top(video_genre_id=genre, ua=ua))
-    elif text.startswith('actor'):
+    elif text.startswith('actor') and len(text[text.index(':')+1:].strip())!=0:
         actor = text[text.index(':')+1:].strip()
-        return 'Actor:\n'+'\n'.join(find_actor(actor_name=actor))
+        return 'Actor:\n'+'\n'.join(find_(name=actor))
+    elif text.startswith('movie') and len(text[text.index(':')+1:].strip())!=0:
+        movie = text[text.index(':')+1:].strip()
+        return 'Movie:\n'+'\n'.join(find_(name=movie, actor=False))
+    return 'There is a mistake in the message'
 
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message_type = update.message.chat.type
     txt = update.message.text
     print(f'User {update.message.chat.id} in {message_type}: "{txt}')
-    if message_type =='group':
+    if message_type == 'group':
         if bot_usname in txt:
             new_text = txt.replace(bot_usname, '').strip()
             response = responses(new_text)
@@ -78,7 +75,6 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 if __name__ == '__main__':
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler('help', help))
-    app.add_handler(CommandHandler('custom', custom))
     app.add_handler(CommandHandler("film_list", tops))
     app.add_handler(CommandHandler('actor', find_information_about_actor))
 
